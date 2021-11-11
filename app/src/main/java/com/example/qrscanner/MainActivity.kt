1038922a7f368626eaf9731128422a351ce7b9b8
kdy1242 <dy12311229@gmail.com>
2021-11-11 19:24:24 +0900

    QR 스캔, 결과 토스트 띄우기

--- app/src/main/java/com/example/qrscanner/MainActivity.kt
+++ app/src/main/java/com/example/qrscanner/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.qrscanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startBarcodeReader(view: View) {
+        IntentIntegrator(this).initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.contents != null) {
+                Toast.makeText(this, "scanned: ${result.contents} format: ${result.formatName}", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
